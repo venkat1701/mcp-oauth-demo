24d8713c577ef4f7435b70dae4e1f538d048e0dd
venkat1701 <thejeastdev@gmail.com>
2025-08-04 17:37:46 +0530

    mcp-oauth-demo-main-project-container

---
+++ src/test/java/io/github/venkat1701/mcpoauthdemo/McpOauthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.venkat1701.mcpoauthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpOauthDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
